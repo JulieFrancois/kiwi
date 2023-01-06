27a7e9413a6b619d1eb46e984a80b416a8e8e203
JulieFrancois <66426143+JulieFrancois@users.noreply.github.com>
2023-01-06 15:55:16 +0100

    comments

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import "./style.css"
 import Header from './components/Header';
-// import Footer from './components/Footer';
+// import Footer from './components/Footer'; à ajouter sur la première page quand le set up sera fait
 import Formulaire from './pages/Formulaire';
 import Hero from './pages/Hero';
 import Formulaire2 from './pages/Formulaire2';
