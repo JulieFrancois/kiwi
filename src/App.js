41a58ab76142832036836e2b31fda1117b231972
minosoa10 <minosoa28@gmail.com>
2023-01-12 16:01:48 +0100

    Création page persona

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import "./style.css"
 import Header from './components/Header';
-// import Footer from './components/Footer';
+// import Footer from './components/Footer'; à ajouter sur la première page quand le set up sera fait
 import Formulaire from './pages/Formulaire';
 import Hero from './pages/Hero';
 import Formulaire2 from './pages/Formulaire2';
 import Formulaire_fini from './pages/Formulaire_fini';
+import Persona from './pages/Persona';
 
 function App() {
 
@@ -27,6 +28,7 @@ function App() {
         {ModifPage === "Form" && <Formulaire setModifPage={setModifPage} setformdata={setformdata}></Formulaire>}
         {ModifPage === "Form2" && <Formulaire2 setModifPage={setModifPage} setformdata={setformdata}></Formulaire2>}
         {ModifPage === "FormF" && <Formulaire_fini setModifPage={setModifPage} setformdata={setformdata}></Formulaire_fini>}
+        {ModifPage === "Persona" && <Persona setModifPage={setModifPage} setformdata={setformdata}></Persona>}
 
 
       </>
