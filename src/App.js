3b282f907856fd9aa5e01b9c778e5f3e13018670
JulieFrancois <66426143+JulieFrancois@users.noreply.github.com>
2023-01-13 11:28:05 +0100

    db fonctionelle

--- src/App.js
+++ src/App.js
@@ -8,31 +8,26 @@ import Hero from './pages/Hero';
 import Formulaire2 from './pages/Formulaire2';
 import Formulaire_fini from './pages/Formulaire_fini';
 import Persona from './pages/Persona';
+import axios from 'axios';
 
 function App() {
 
   // JAVASCRIPT
   const[ModifPage,setModifPage] = React.useState("App"); 
-  const[formdata,setformdata,] = React.useState(); 
+  const[formdata,setformdata,] = React.useState();
+  const [DATABASE,setDATABASE] = React.useState([]);
 
   // const [Genre, setGenre] = React.useState("Femme");()
 
   React.useEffect(()=>{
-     console.log(postData("https://illustrious-cat-7fb4d9.netlify.app/api/persona"))
+    //  console.log(postData("https://illustrious-cat-7fb4d9.netlify.app/api/persona"))
+
+    axios
+      .get("https://illustrious-cat-7fb4d9.netlify.app/api/persona")
+      .then((response) => response)
+      .then((data) =>  setDATABASE(data.data))
   },[])
-    async function postData(url = '', data = {}) {
-      const response = await fetch(url , {
-        method: 'GET', // *GET, POST, PUT, DELETE, etc.
-        mode: 'no-cors', 
-        cache: 'no-cache', 
-        headers: {
-          'Content-Type': 'application/json'
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        // body: JSON.stringify(data) // body data type must match "Content-Type" header
-      });
-      return response; // parses JSON response into native JavaScript objects
-    }
+
   //JSX
   return (
       <>
